19c93b3912edb4031463415de6169e4cfacbf37e
rangga saputra <ranggasaputra200412@gmail.com>
2021-02-27 22:17:41 +0700

    Adding Paragrafh in Php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Try Git Remote</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt provident laudantium ipsam optio suscipit molestias porro, distinctio ipsa necessitatibus omnis iste ipsum deleniti eius? Blanditiis eos autem molestiae neque deleniti.</P>
+
+</body>
+
+</html>
